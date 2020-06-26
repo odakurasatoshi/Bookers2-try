e7611cb4bc2f9184c264b76a7daa5205bfe5fd48
vagrant <vagrant@localhost.localdomain>
2020-06-26 01:20:29 +0000

    [Add]Bookers2-try投稿機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,23 @@
 class BooksController < ApplicationController
-	def new
-	end
 
 	def create
+		@book = Book.new(book_params)
+		@book.user_id = current_user.id #ここがないと保存されず、入力前の画面に戻るのは何故？１対Nの設定と関係あり？
+		@book.save
+		redirect_to books_path
 	end
 
 	def index
+		@book = Book.new
+		@books = Book.all
 	end
 
 	def show
 	end
+
+	private
+	def book_params
+		params.require(:book).permit(:title, :body)
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'homes#top'
-  resources :books,only: [:new, :create, :show, :index]
+  get 'home/about',  to: 'homes#about', as: 'home_about'
+  resources :books,only: [:create, :show, :index]
 end
